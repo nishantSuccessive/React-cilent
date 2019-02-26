ff133e9b9557a88878ff66b3f3bc30956af3d6c2
nishantSuccessive <nishant.saxena@successive.tech>
2019-02-26 20:26:47 +0530

    docs: created navbar and login component with the help of material ui

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,3 +2,5 @@ export { TextFieldDemo } from './TextFieldDemo';
 export { InputDemo } from './InputDemo';
 export { ChildrenDemo } from './ChildrenDemo';
 export { Trainee } from './Trainee';
+export { Login } from './Login';
+export { Navbar } from './component';
