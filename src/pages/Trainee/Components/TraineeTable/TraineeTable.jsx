782b2340da9d0a0cc5ab10eb5fab9d189c114aaa
nishantSuccessive <nishant.saxena@successive.tech>
2019-03-04 19:34:23 +0530

    docs: created tables and applied routing and also used onsort handler

--- src/pages/Trainee/Components/TraineeTable/TraineeTable.jsx
+++ src/pages/Trainee/Components/TraineeTable/TraineeTable.jsx
@@ -7,7 +7,7 @@ import TableCell from '@material-ui/core/TableCell';
 import TableHead from '@material-ui/core/TableHead';
 import TableRow from '@material-ui/core/TableRow';
 import Paper from '@material-ui/core/Paper';
-import { rows } from '../../index';
+import TableSortLabel from '@material-ui/core/TableSortLabel';
 
 const styles = theme => ({
   root: {
@@ -18,6 +18,12 @@ const styles = theme => ({
   table: {
     minWidth: 100,
   },
+  row: {
+    '&:nth-of-type(odd)': {
+      backgroundColor: theme.palette.background.default,
+    },
+    cursor: 'pointer',
+  },
 });
 
 class Traineetable extends React.Component {
@@ -26,25 +32,62 @@ class Traineetable extends React.Component {
     this.state = {};
   }
 
+  handleChange = property => (event) => {
+    const { onSort } = this.props;
+    onSort(event, property);
+  }
+
+  handleClick = property => (event) => {
+    const { onSelect } = this.props;
+    onSelect(event, property);
+  }
+
   render() {
+    const {
+      columns, data, orderBy, order,
+    } = this.props;
     const { classes } = this.props;
     return (
       <Paper className={classes.root}>
         <Table className={classes.table}>
+
           <TableHead>
             <TableRow>
-              <TableCell align="center">Name</TableCell>
-              <TableCell align="center">Email</TableCell>
+              {
+                columns.map(element => (
+
+                  <TableCell
+                    align={element.align}
+                    sortDirection={orderBy === element.field ? order : false}
+                  >
+                    <TableSortLabel
+                      active={orderBy === element.field}
+                      direction={order}
+                      onClick={this.handleChange(element.field)}
+                    >
+                      {element.label}
+
+                    </TableSortLabel>
+
+                  </TableCell>
+                ))
+              }
+
             </TableRow>
+
           </TableHead>
+
           <TableBody>
-            {rows.map(row => (
-              <TableRow key={row.id}>
-                <TableCell align="center" component="th" scope="row">
-                  {row.name}
-                </TableCell>
-                <TableCell align="center">{row.email}</TableCell>
+            {data.map(row => (
+              <TableRow key={row.id} onClick={this.handleClick(row.id)} className={classes.row} hover>
+                {columns.map(column => (
+                  <TableCell align={column.align} component="th" scope="row">
+                    { (column.format) ? column.format(row[column.field]) : row[column.field]}
+                  </TableCell>
+                ))
+                }
               </TableRow>
+
             ))}
           </TableBody>
         </Table>
@@ -54,6 +97,18 @@ class Traineetable extends React.Component {
 }
 Traineetable.propTypes = {
   classes: PropTypes.node.isRequired,
+  columns: PropTypes.objectOf.isRequired,
+  data: PropTypes.objectOf.isRequired,
+  orderBy: PropTypes.string,
+  order: PropTypes.string,
+  onSort: PropTypes.func.isRequired,
+  onSelect: PropTypes.func.isRequired,
+
+};
+
+Traineetable.defaultProps = {
+  orderBy: '',
+  order: 'asc',
 };
 
 export default withStyles(styles)(Traineetable);
--- src/pages/Trainee/TraineeList.jsx
+++ src/pages/Trainee/TraineeList.jsx
@@ -3,6 +3,8 @@ import {
   Link,
 } from 'react-router-dom';
 import Button from '@material-ui/core/Button';
+import moment from 'moment';
+import PropTypes from 'prop-types';
 import { trainees } from './data';
 import { AddDialog } from './Components';
 import { styleForButton } from './style';
@@ -14,6 +16,8 @@ export class TraineeList extends React.Component {
     super(props);
     this.state = {
       open: false,
+      order: 'desc',
+      orderBy: '',
     };
   }
 
@@ -21,13 +25,37 @@ export class TraineeList extends React.Component {
     this.setState({ open: true });
   };
 
+
+  handleSort = (event, property) => {
+    // const orderBy = property;
+    // let order = 'desc';
+    const { orderBy, order } = this.state;
+    if (orderBy === property && order === 'asc') {
+      return this.setState({ order: 'desc', orderBy: property });
+    }
+
+    return this.setState({ order: 'asc', orderBy: property });
+  };
+
+  getDateFormatted = (value) => {
+    const formattedDate = moment(value).format('dddd, MMMM Do YYYY, h:mm:ss ');
+    return formattedDate;
+  }
+
   handleClose = (name, email, password) => {
     this.setState({ open: false });
     console.log(name, email, password);
   };
 
+  handleSelect = (event, property) => {
+    const { history } = this.props;
+    history.replace(`/trainee/${property}`);
+  };
+
   render() {
-    const { open, fullWidth, maxWidth } = this.state;
+    const {
+      open, fullWidth, maxWidth, order, orderBy,
+    } = this.state;
 
     const items = trainees.map(item => <li><Link to={`/trainee/${item.id}`}>{item.name}</Link></li>);
     return (
@@ -41,7 +69,29 @@ export class TraineeList extends React.Component {
           fullWidth={fullWidth}
           maxWidth={maxWidth}
         />
-        <TraineeTable />
+        <TraineeTable
+          data={trainees}
+          columns={[{
+            field: 'name',
+            label: 'Name',
+          },
+          {
+            field: 'email',
+            label: 'Email Address',
+            format: value => value && value.toUpperCase(),
+          },
+          {
+            field: 'createdAt',
+            label: 'Date',
+            align: 'right',
+            format: this.getDateFormatted,
+          },
+          ]}
+          orderBy={orderBy}
+          order={order}
+          onSort={this.handleSort}
+          onSelect={this.handleSelect}
+        />
         <div>
           <ul>
             {items}
@@ -51,3 +101,6 @@ export class TraineeList extends React.Component {
     );
   }
 }
+TraineeList.propTypes = {
+  history: PropTypes.func.isRequired,
+};
